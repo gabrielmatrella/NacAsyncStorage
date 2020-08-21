25128326a74337cec276472ad037522e87ee9a6b
Gabriel <matrella2@gmail.com>
2020-08-21 14:37:53 -0300

    feature/forgot-pwd-screen-nav

--- App.js
+++ App.js
@@ -12,16 +12,24 @@ const LoginScreen = ({navigation}) => {
   return (
     <View>
       <Button title="Logar" onPress={()=> {navigation.navigate('Home', {})}}></Button>
-      <Button title="Não é registrado? Clique aqui" onPress={()=> {navigation.navigate('Register', {})}}></Button>
+      <Button title="Cadastre-se" onPress={()=> {navigation.navigate('Register', {})}}></Button>
+      <Button title="Esqueceu sua senha? Clique aqui" onPress={()=> {navigation.navigate('ForgotPassword', {})}}></Button>
     </View>
   );
 }
+
 const HomeScreen = () => {
   return <Text>Seja bem vindo: ((Preencher com usuario logado do asyncstorage))</Text>;
 };
+
 const RegisterScreen = () => {
   return <Text>Criar formulario de registro com campos nome,email,senha,confirmar senha</Text>;
 };
+
+const ForgotPasswordScreen = () => {
+  return <Text>Formulario para criar nova senha para o usuario</Text>;
+};
+
 const App = () => {
   return (
     <NavigationContainer>
@@ -29,6 +37,7 @@ const App = () => {
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Home" component={HomeScreen} options={{title:"Bem vindo"}}/>
       <Stack.Screen name="Register" component={RegisterScreen} options={{title:"Cadastro"}}/>
+      <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} options={{title:"Recuperar senha"}}/>
     </Stack.Navigator>
   </NavigationContainer>
   );
@@ -36,8 +45,5 @@ const App = () => {
 
 const styles = StyleSheet.create({
 });
-
-
-
 
 export default App;
